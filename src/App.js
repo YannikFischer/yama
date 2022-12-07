fdcc374fe2d61ba608744560d733b467167942db
JaytheKK <yannik.fischer@student.htldornbirn.at>
2022-12-07 10:45:14 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-
+    <div id="app">
+      <BrowserRouter>
+        {/* <Navbar /> */}
+        <Routes>
+          {/* <Route path='/' element={<MainPage />} />
+          <Route path='/lost' element={<Lost />} />
+          <Route path='/found' element={<Found />} />
+          <Route path='/items/:id' element={<Preview />} />
+          <Route path='*' element={() => <Navigate to='/' />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
