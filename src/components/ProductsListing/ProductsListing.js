8732adbe0dedf3a0619a00a336b70419c85ee329
JaytheKK <yannik.fischer@student.htldornbirn.at>
2023-03-21 09:23:25 +0100

    products

---
+++ src/components/ProductsListing/ProductsListing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./ProductsListing.scss"
+
+const ProductsListing = () => {
+    return (
+        <div className='main'>
+            <div className='productContainer'>
+                <h1 className='header'>Smartphones</h1>
+                <div className='product'></div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductsListing
